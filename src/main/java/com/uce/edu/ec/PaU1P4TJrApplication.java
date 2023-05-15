9929f47207d807f491134b072123867ba235e37d
John <johnfe@outlook.es>
2023-05-14 21:37:05 -0500

    1er commit

---
+++ src/main/java/com/uce/edu/ec/PaU1P4TJrApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaU1P4TJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaU1P4TJrApplication.class, args);
+		System.out.println("Creacion del proyecto");
+	}
+}
